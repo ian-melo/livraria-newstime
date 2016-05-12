abe8bcafa99e5e94667959da90e57c7897172917
Usuario <Usuario@Fa-PC>
2016-05-12 19:11:42 -0300

    ControleAdiministracao, alterado

--- src/newstime/controle/ControleAdministracao.java
+++ src/newstime/controle/ControleAdministracao.java
@@ -1,5 +1,10 @@
 package newstime.controle;
 
+import newstime.entidade.Autor;
+import newstime.entidade.Editora;
+import newstime.entidade.Livro;
+import newstime.excecao.NegocioException;
+
 
 /**
  * Está classe controla o cadastro dos livro, verifica se todos os requisitos e 
@@ -41,6 +46,8 @@ public class ControleAdministracao {
      * @param editora
      * @param anoPublicacao
      * @param categoria
+     * @param resumo
+     * @param sumario
      * @param qtdEstoque
      * @param precoVenda
      * @param precoOferta
@@ -48,10 +55,34 @@ public class ControleAdministracao {
      * @param margemLucro
      * @param oferta
      * @param digital
+     * @throws newstime.excecao.NegocioException
 
      */
-    public void inserirLivro(String isbn, String titulo, String autor, String editora, String anoPublicacao, String categoria, String resumo, String sumario, int qtdEstoque, String precoVenda, String precoOferta, String precoCusto, String margemLucro, boolean oferta, boolean digital){
+    public void inserirLivro(String isbn, String titulo, Autor autor, Editora editora, String anoPublicacao, String categoria, String resumo, String sumario, int qtdEstoque, String precoVenda, String precoOferta, String precoCusto, String margemLucro, boolean oferta, boolean digital) throws NegocioException{
+        //Mudança no diagrama de tipo string autor para tipo Autor autor;
+        //Mudança no diagrama de tipo string editora para tipo Editora editora;
+      
+        Livro livro = new Livro();
+        livro.setIsbn(isbn);
+        livro.setTitulo(titulo);
         
+        livro.setAutor(autor);// objeto tipo autor
+        livro.setEditora(editora);
+        
+        livro.setAnoPublicacao(qtdEstoque);
+        livro.setCategoria(Livro.CategoriaLivro.ARTES);//mudar verificar//erro
+        
+        livro.setResumo(resumo);
+        livro.setSumario(sumario);
+        livro.setQtdEstoque(qtdEstoque);
+        livro.setPrecoVenda(Float.parseFloat(precoVenda));
+        livro.setPrecoOferta(Float.parseFloat(precoOferta));
+        livro.setPrecoCusto(Float.parseFloat(precoCusto));
+        livro.setMargemLucro(Float.parseFloat(margemLucro));
+        livro.setOferta(oferta);
+        livro.setDigital(digital);
+        
+       
     }
     
     
